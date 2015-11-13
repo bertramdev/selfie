af7d6ad187cdb0c2276da5071722e70088bebf95
Philipp Bohnenstengel (raumobil GmbH) <p.bohnenstengel@raumobil.com>
2015-11-13 13:23:08 +0100

    added comment

--- src/groovy/com/bertramlabs/plugins/selfie/Attachment.groovy
+++ src/groovy/com/bertramlabs/plugins/selfie/Attachment.groovy
@@ -192,7 +192,7 @@ class Attachment {
 		def extension 
 		try {
 			extension = (fileName =~ /[.]([^.]+)$/)[0][1]
-			"${fileNameWithOutExt}_${typeName}.${extension?.toLowerCase()}"
+			"${fileNameWithOutExt}_${typeName}.${extension?.toLowerCase()}" //uppercase extensions prevent correct content type detection
 		} catch (IndexOutOfBoundsException e) {
 			//file has no extension
 			"${fileNameWithOutExt}_${typeName}"
