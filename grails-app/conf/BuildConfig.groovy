76b17b0cfa89a56d0b099e60803dac17f982b4fe
David Estes <davydotcom@gmail.com>
2015-12-21 07:45:15 -0500

    fixing resize bug for fit mode

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -20,7 +20,7 @@ grails.project.dependency.resolution = {
             runtime(':hibernate:3.6.10.16') {
                 export = false
             }
-            build(":release:3.0.1",":rest-client-builder:1.0.3") {
+            build(":release:3.1.2",":rest-client-builder:1.0.3") {
                 export = false
             }
         }
